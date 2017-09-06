ee2b034046eaee0e7c2cd0fef8c9b08eb7baa825
LindvedKrvang <rklindved@gmail.com>
2017-09-06 11:35:47 +0200

    Added a profile converter

---
+++ VideoMenuBLL/Converters/ProfileConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VideoMenuBLL.BusinessObjects;
+using VideoMenuDAL.Entities;
+
+namespace VideoMenuBLL.Converters
+{
+    class ProfileConverter
+    {
+        internal ProfileBO Convert(Profile profile)
+        {
+            return new ProfileBO()
+            {
+                Id = profile.Id,
+                FirstName = profile.FirstName,
+                LastName = profile.LastName,
+                Address = profile.Address,
+            };
+        }
+
+        internal Profile Convert(ProfileBO profile)
+        {
+            return new Profile()
+            {
+                Id = profile.Id,
+                FirstName = profile.FirstName,
+                LastName = profile.LastName,
+                Address = profile.Address
+            };
+        }
+    }
+}
