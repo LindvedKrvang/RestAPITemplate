015956fa00d9bfcc93028160709fabf18dcb58a0
LindvedKrvang <rklindved@gmail.com>
2017-09-06 10:01:33 +0200

    Added entity profile and BO

---
+++ VideoMenuBLL/BusinessObjects/ProfileBO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VideoMenuBLL.BusinessObjects
+{
+    public class ProfileBO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+
+        public ProfileBO()
+        {
+            
+        }
+
+        public string FullName => $"{FirstName} + {LastName}";
+    }
+}
