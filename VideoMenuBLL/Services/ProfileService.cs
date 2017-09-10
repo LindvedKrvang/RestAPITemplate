962f6f45265efcaebb539d44b6a8e4ef86fe743e
LindvedKrvang <rklindved@gmail.com>
2017-09-10 19:16:41 +0200

    Prepared for unitTest for ProfileService

---
+++ VideoMenuBLL/Services/ProfileService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VideoMenuBLL.BusinessObjects;
+using VideoMenuBLL.Converters;
+using VideoMenuDAL;
+
+namespace VideoMenuBLL.Services
+{
+    public class ProfileService : IService<ProfileBO>
+    {
+        private readonly ProfileConverter _converter = new ProfileConverter();
+        private readonly IDalFacade _facade;
+
+        public ProfileService(IDalFacade facade)
+        {
+            _facade = facade;
+        }
+
+
+        public ProfileBO Create(ProfileBO entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProfileBO> CreateAll(List<ProfileBO> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProfileBO> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProfileBO GetOne(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProfileBO> Search(string searchQuery)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(ProfileBO video)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProfileBO Delete(int idOfVideo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ClearAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ VideoMenuBLLTest/ProfileServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VideoMenuBLL;
+using VideoMenuBLL.BusinessObjects;
+using VideoMenuBLL.Services;
+using VideoMenuDAL;
+
+namespace VideoMenuBLLTest
+{
+    public class ProfileServiceTests
+    {
+        private readonly IService<ProfileBO> _service;
+
+        public ProfileServiceTests()
+        {
+            IDalFacade facade = new DalFacade();
+            _service = new ProfileService(facade);
+            _service.ClearAll();
+        }
+    }
+}
